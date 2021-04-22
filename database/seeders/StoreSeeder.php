c6794ad1f6ecb7e31048ffc84ddeab1e32fe9de8
Arya <aryaashari100@gmail.com>
2021-04-22 22:51:07 +0900

    membuat seeder untuk store

---
+++ database/seeders/StoreSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Store;
+
+class StoreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Store::create([
+            'user_id' => 1,
+            'nama_toko' => 'Toko Arya',
+            'no_telp_toko' => '08123456788',
+            'akun_instagram' => 'aryyashari',
+            'alamat_toko' => 'Jl.pasar lama sentani',
+            'deskripsi_toko' => 'Toko ini menjual peralatan-peralatan yang anda butuhkan sehari-hari',
+            'foto_profile_toko' => 'profile_toko.png'
+        ]);
+    }
+}
