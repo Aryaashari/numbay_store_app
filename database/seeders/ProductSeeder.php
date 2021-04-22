42e22baf653f7fd9dbdec6d83abc937d6bcac0fd
Arya <aryaashari100@gmail.com>
2021-04-22 23:00:31 +0900

    membuat seeder untuk product

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::create([
+            'store_id' => 1,
+            'category_id' => 1,
+            'nama_produk' => 'Coto Makassar',
+            'harga_produk' => 30000,
+            'deskripsi_produk' => 'Makanan Enak',
+            'foto_produk' => 'coto.jpg'
+        ]);
+
+        Product::create([
+            'store_id' => 1,
+            'category_id' => 1,
+            'nama_produk' => 'Siomay Maknyus',
+            'harga_produk' => 10000,
+            'deskripsi_produk' => 'Makanan Enak',
+            'foto_produk' => 'somay.png'
+        ]);
+
+        Product::create([
+            'store_id' => 1,
+            'category_id' => 3,
+            'nama_produk' => 'Kangkung Hidroponik',
+            'harga_produk' => 10000,
+            'deskripsi_produk' => 'Kangkung bagus',
+            'foto_produk' => 'kangkung.png'
+        ]);
+    }
+}
