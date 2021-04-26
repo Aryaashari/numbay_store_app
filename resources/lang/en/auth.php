fb4148acaa40608d8141cb4ef2dc42072ace69dd
Arya <aryaashari100@gmail.com>
2021-04-26 21:17:18 +0900

    mengubah tulisan validasi login user

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'Password atau username salah!',
+    'failed' => 'Username atau password salah!',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
