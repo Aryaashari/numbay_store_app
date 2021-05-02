5436540756c5ab117fca89bad4c62fc9b5aebc43
Arya <aryaashari100@gmail.com>
2021-05-02 20:58:26 +0900

    membuat widget total user, total toko, dan total produk pada dashboard admin

--- resources/views/dashboard/admin-dashboard.blade.php
+++ resources/views/dashboard/admin-dashboard.blade.php
@@ -1,1 +1,88 @@
 @extends('layouts.dashboard')
+
+
+@section('title', 'Numbay Store - Admin|Dashboard')
+@section('title-page', 'Dashboard')
+
+
+@push('css')
+    <link rel="stylesheet" type="text/css" href="{{ asset('dashboard/css/widgets/modules-widgets.css') }}">
+@endpush
+
+@push('js')
+    <script src="{{ asset('dashboard/js/widgets/modules-widgets.js') }}"></script> 
+@endpush
+
+
+
+@section('content')
+
+<div class="row mt-3">
+    <div class="col-12 col-md-4 mb-3">
+        <div class="widget widget-five widget-activity-one">
+            <div class="widget-content">
+
+                <div class="header align-items-center">
+                    <div class="header-body">
+                        <h6>Total User</h6>
+                    </div>
+                </div>
+
+                <div class="w-content">
+                    <div class="">                                            
+                        <p class="task-left">12</p>
+                    </div>
+                    <div class="tm-action-btn">
+                        <a href="#" class="btn">Lihat User</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12 col-md-4 mb-3">
+        <div class="widget widget-five widget-activity-one">
+            <div class="widget-content">
+
+                <div class="header align-items-center">
+                    <div class="header-body">
+                        <h6>Total Toko</h6>
+                    </div>
+                </div>
+
+                <div class="w-content">
+                    <div class="">                                            
+                        <p class="task-left">12</p>
+                    </div>
+                    <div class="tm-action-btn">
+                        <a href="#" class="btn">Lihat Toko</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-12 col-md-4 mb-3">
+        <div class="widget widget-five widget-activity-one">
+            <div class="widget-content">
+
+                <div class="header align-items-center">
+                    <div class="header-body">
+                        <h6>Total Produk</h6>
+                    </div>
+                </div>
+
+                <div class="w-content">
+                    <div class="">                                            
+                        <p class="task-left">12</p>
+                    </div>
+                    <div class="tm-action-btn">
+                        <a href="#" class="btn">Lihat Produk</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
