eb75b18b65a5ed73f3ddd8bea5b8fc7898176fa0
Arya <aryaashari100@gmail.com>
2021-05-03 21:31:16 +0900

    membuat kategori Lainnya berada selalu paling terakhir pada halaman Tambah produk

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -35,7 +35,25 @@ class ProductController extends Controller
 
 
     public function create() {
-        $categories = Category::all();
+        $categories = [];
+        $kategoriLainnya;
+
+        // Looping semua kategori
+        foreach (Category::all() as $category) {
+
+            // Jika kategori merupakan kategori Lainnya, maka masukkan ke variable kategoriLainnya
+            if ($category->kategori == 'Lainnya') {
+                $kategoriLainnya = $category;
+
+            // Jika bukan masukkan ke dalam array categories 
+            } else {
+                $categories[] = $category;
+            }
+        }
+
+        // Masukkan kategoriLainnya kedalam array categories, supaya kategoriLainnya berada selalu paling terakhir
+        $categories[] = $kategoriLainnya;
+
         return view('dashboard.product.create', compact('categories'));
     }
 
