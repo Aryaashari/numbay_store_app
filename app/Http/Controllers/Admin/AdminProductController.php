132ae314d10d0bdaa8672783cc3fc6b8cdb3d1f1
Arya <aryaashari100@gmail.com>
2021-05-03 21:49:00 +0900

    membuat route untuk product di dashboard admin

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class AdminProductController extends Controller
 {
-    //
+    public function index() {
+        $products = Product::all();
+
+        return view('dashboard.product.index', compact('products'));
+    }
 }
