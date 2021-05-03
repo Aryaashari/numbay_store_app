60b18cca80bfa3470f03250d2b30def39ef4fe7e
Arya <aryaashari100@gmail.com>
2021-05-03 21:53:54 +0900

    membuat file AdminProductController

---
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminProductController extends Controller
+{
+    public function index() {
+        $products = Product::all();
+
+        return view('dashboard.product.index', compact('products'));
+    }
+}
