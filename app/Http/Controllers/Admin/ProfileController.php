e3f98d89f57b0272c966d32ed65ab6df6eb79ddd
Arya <aryaashari100@gmail.com>
2021-05-07 14:02:50 +0900

    membuat controller untuk halaman edit profile

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class ProfileController extends Controller
 {
-    //
+    public function edit() {
+        $user = auth()->user();
+
+        return view('dashboard.admin.edit-profile', compact('user'));
+    }
 }
