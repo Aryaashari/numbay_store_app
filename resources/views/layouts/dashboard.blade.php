c1e167bf1630337716eee460556cee1ab0d19dfd
Arya <aryaashari100@gmail.com>
2021-07-20 07:29:27 +0900

    menambahkan menu user pada sidebar

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -183,6 +183,18 @@
                             </a>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
                         </li>
+
+                        <li class="menu {{ (request()->is('admin/orders') || request()->is('admin/orders/*')) ? 'active' : '' }}">
+                            <a href="{{ url('/admin/orders') }}" data-active="{{ (request()->is('admin/orders') || request()->is('admin/orders/*')) ? 'true' : 'false' }}" class="menu-toggle">
+                                <div class="base-menu">
+                                    <div class="base-icons">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+                                    </div>
+                                    <span>Pengguna</span>
+                                </div>
+                            </a>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
+                        </li>
                     @endif
 
                 </ul>
