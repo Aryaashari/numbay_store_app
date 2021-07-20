6e26a4e7e5e541c9b30eb32105b52dc9ab96420c
Arya <aryaashari100@gmail.com>
2021-07-21 06:11:35 +0900

    mengubah bagian https protocol middlewarw

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,10 +16,10 @@ class HttpsProtocol
      */
     public function handle($request, Closure $next)
     {
-            if (!$request->secure()) {
-               return redirect()->secure($request->getRequestUri());
-            }
+            // if (!$request->secure()) {
+            //    return redirect()->secure($request->getRequestUri());
+            // }
 
-            return $next($request); 
+            // return $next($request); 
     }
 }
