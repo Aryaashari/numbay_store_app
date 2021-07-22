3b45964b5b8b4f065177f8cc43c4a5887c7f13be
Arya <aryaashari100@gmail.com>
2021-07-22 16:13:53 +0900

    membuat hover produk

--- resources/views/product/detail-product.blade.php
+++ resources/views/product/detail-product.blade.php
@@ -53,7 +53,11 @@
             <div class="row">
                 <div class="col-lg-6 col-12">
                     <div class="img-card">
-                        <div class="img" style="background-image: url({{ asset('storage/uploads/product/'.$product->foto_produk) }});"></div>
+                        <div class="img" style="background-image: url({{ asset('storage/uploads/product/'.$product->foto_produk) }});">
+                            <div class="img-overlay">
+                                <p>Lihat gambar...</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="col-lg-6 col-12">
