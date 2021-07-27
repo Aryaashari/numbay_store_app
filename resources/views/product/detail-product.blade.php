c47b9498f65d6d6ee094f7283b65e91c0e53c4eb
Arya <aryaashari100@gmail.com>
2021-07-27 22:19:55 +0900

    membuat counter angka jumlah pesanan

--- resources/views/product/detail-product.blade.php
+++ resources/views/product/detail-product.blade.php
@@ -77,7 +77,7 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
                                 </div>
                                 <div class="angka">
-                                    <p style="margin-top: 18px">15</p>
+                                    <p style="margin-top: 18px">1</p>
                                 </div>
                                 <div class="kurang">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-minus"><line x1="5" y1="12" x2="19" y2="12"></line></svg>
@@ -184,6 +184,26 @@
                 
 
             }
+
+
+
+            // === Jumlah pesanan ===
+            let jumlahPesanan = 1;
+
+            // Ketika tombol tambah diklik
+            $('.tambah').click(function () { 
+                $('.angka p').html(++jumlahPesanan);
+                $('.angka p').val(jumlahPesanan);
+            });
+
+            // Ketika tombol kurang diklik
+            $('.kurang').click(function () { 
+                if (jumlahPesanan > 1) {
+                    $('.angka p').html(--jumlahPesanan);
+                    $('.angka p').val(jumlahPesanan);
+                }
+            });
+
 
 
         });
