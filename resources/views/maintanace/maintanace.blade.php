af23f5527af9236da6611ac20ffb45ad9a12fcc8
Arya <aryaashari100@gmail.com>
2021-08-12 15:25:33 +0900

    membuat halaman maintanance mode

--- resources/views/maintanace/maintanace.blade.php
+++ resources/views/maintanace/maintanace.blade.php
@@ -4,26 +4,31 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-    <title>Numbay Store - Error @yield('error_code')</title>
+    <title>Numbay Store - Pemeliharaan dan pengembangan</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('dashboard/img/numbaystore.png') }}"/>
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
     <link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('dashboard/css/plugins.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('dashboard/css/structure.css') }}" rel="stylesheet" type="text/css" class="structure" />
-    <link href="{{ asset('dashboard/css/pages/error/style-400.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('dashboard/css/pages/error/style-maintanence.css') }}" rel="stylesheet" type="text/css" />
     <!-- END GLOBAL MANDATORY STYLES -->
-    
+
 </head>
-<body class="error404 text-center">
-    <div class="container-fluid error-content">
+<body class="maintanence text-center">
+    
+    <div class="container-fluid maintanence-content">
         <div class="">
-            <h1 class="error-number">@yield('error_code')</h1>
-            <p class="mini-text">Ooops!</p>
-            <p class="error-text mb-4 mt-1">@yield('error_message')</p>
-            <a class="btn btn-primary mt-5" onclick="window.history.back();">Kembali</a>
+            <div>
+                <img alt="logo" src="{{ asset('frontend/img/logo.png') }}" width="300">
+            </div>
+            <h1 class="error-title" style="font-size: 40px">Sedang Pemeliharaan dan Pengembangan</h1>
+            <p class="error-text">Terima kasih telah mengakses website kami.</p>
+            <p class="text">Saat ini website Numbay Store sedang dalam proses pemeliharaan dan pengembangan, <br> yang bertujuan untuk meningkatkan peforma website dan menambahkan beberapa fitur terbaru. <br> Mohon maaf atas ketidaknyamanan ini.</p>
+            <p class="text">Silahkan kembali beberapa saat lagi.</p>
         </div>
-    </div>    
+    </div>
+    
     <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
     <script src="{{ asset('js/libs/jquery-3.4.1.min.js')}}"></script>
     <script src="{{ asset('js/popper.min.js') }}"></script>
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,9 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\Admin\AdminUserController;
 
-
+Route::get('/maintanace', function() {
+    return view('maintanace.maintanace');
+});
 
 // Home
 Route::get('/', [HomeController::class, 'index']);
